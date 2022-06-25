718b14c3544448ed3045c41118b1ffab39dd8abb
wiji <wiji.permana@gmail.com>
2022-06-25 08:40:07 +0700

    Finish edit feature

--- src/App.jsx
+++ src/App.jsx
@@ -37,13 +37,40 @@ function App() {
     setTodoList(prevTodos => prevTodos.filter(todo => todo.id !== id));
   };
 
+  const handleClickEditTodo = id => {
+    setTodoList(prevTodos =>
+      prevTodos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isEditing: !todo.isEditing,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleChangeTodo = (id, event) => {
+    if (event.key === "Enter") {
+      setTodoList(prevTodos =>
+        prevTodos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              text: event.target.value,
+              isEditing: !todo.isEditing,
+            };
+          }
+          return todo;
+        })
+      );
+    }
+  };
+
   const handleClickClearTodos = () => {
     setTodoList(prevTodos => prevTodos.filter(todo => !todo.isCompleted));
   };
-
-  useEffect(() => {
-    console.log(todoList);
-  }, [todoList]);
 
   return (
     <>
@@ -76,24 +103,50 @@ function App() {
               <>
                 {todoList.map(todo => (
                   <div className='Todo' key={todo.id}>
-                    <div className='Right'>
-                      <input
-                        type='checkbox'
-                        onChange={event =>
-                          handleOnChangeCheckTodo(event, todo.id)
-                        }
-                      />
-                      <div className={`Text ${todo.isCompleted && "Checked"}`}>
-                        {todo.text}
+                    {todo.isEditing ? (
+                      <div className='Editing'>
+                        <input
+                          type='text'
+                          onKeyUp={event => {
+                            handleChangeTodo(todo.id, event);
+                          }}
+                        />
+                        <button onClick={() => handleClickEditTodo(todo.id)}>
+                          cancel
+                        </button>
                       </div>
-                    </div>
+                    ) : (
+                      <>
+                        <div className='Right'>
+                          <input
+                            type='checkbox'
+                            onChange={event =>
+                              handleOnChangeCheckTodo(event, todo.id)
+                            }
+                          />
+                          <div
+                            className={`Text ${todo.isCompleted && "Checked"}`}
+                          >
+                            {todo.text}
+                          </div>
+                        </div>
 
-                    <div className='Left'>
-                      <button>edit</button>
-                      <button onClick={() => handleClickRemoveTodo(todo.id)}>
-                        remove
-                      </button>
-                    </div>
+                        <div className='Left'>
+                          <button
+                            className={`${todo.isCompleted && "Disabled"}`}
+                            onClick={() => handleClickEditTodo(todo.id)}
+                            disabled={todo.isCompleted}
+                          >
+                            edit
+                          </button>
+                          <button
+                            onClick={() => handleClickRemoveTodo(todo.id)}
+                          >
+                            remove
+                          </button>
+                        </div>
+                      </>
+                    )}
                   </div>
                 ))}
               </>
